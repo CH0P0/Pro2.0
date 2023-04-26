4cde45061527a0925002d39c8337446a1a37089c
CH0P0 <gafonsoudev@gmail.com>
2023-04-26 08:53:23 +0100

    Clase File empezada

---
+++ AppSim/AppSim/FileDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSim
+{
+    internal class FileDB
+    {
+        public const string FILENAME = "userDatabase.csv";
+
+        public static bool Exist() => File.Exists(FILENAME);
+
+        public static bool MakeFile()
+        {
+            try
+            {
+                if (!Exist())
+                    File.Create(FILENAME).Close();
+                Console.WriteLine("Archivo creado con exito");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
