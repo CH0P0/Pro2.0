5b9958bf866d7dd78bca59b792a02ba837dfb689
CH0P0 <gafonsoudev@gmail.com>
2023-04-28 17:19:12 +0100

    Cifrado de contraseñas terminado

--- AppSim/AppSim/User.cs
+++ AppSim/AppSim/User.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -121,6 +123,57 @@ namespace AppSim
         }
 
         /// <summary>
+        /// Método que cifra la contraseña del usuario antes
+        /// de guardarla en la base de datos para evitar 
+        /// filtraciones
+        /// </summary>
+        /// <param name="movement">
+        /// número importante para esta función, por
+        /// seguridad no se dara más especificaciones
+        /// </param>
+        /// <returns>La contraseña cifrada</returns>
+        public string Cipher(int movement)
+        {
+                string cipherPassword = "";
+                foreach (char c in GetPassword())
+                    if (char.IsLetter(c)) // Si el caracter es una letra, se cifra mediante el desplazamiento
+                        cipherPassword += (char)(((int)c + movement - 65) % 26 + 65); // Agregar la letra cifrada al resultado
+                    else if (char.IsDigit(c)) // Si el caracter es un número, se cifra mediante el desplazamiento
+                        cipherPassword += (char)(((int)c + movement - 48) % 10 + 48); // Agregar el número cifrado al resultado
+                    else // Si el caracter no es una letra ni un número, se agrega tal cual al resultado
+                        cipherPassword += c;
+            cipherPassword += movement;
+                return cipherPassword; // Devolver el resultado del cifrado
+        }
+
+        /// <summary>
+        /// Método que descifra la contraseña del usuario antes
+        /// de iniciar iniciar el programa
+        /// </summary>
+        /// <param name="movement">
+        /// número importante para esta función, por
+        /// seguridad no se dara más especificaciones
+        /// </param>
+        /// <returns>La contraseña descifrada</returns>
+        public string UnCipher(int movement)
+        {
+            string uncipherPassword = ""; // Variable para almacenar el resultado
+
+            foreach (char c in GetPassword())
+                if (char.IsLetter(c)) // Si el caracter es una letra, se descifra mediante el desplazamiento
+                    uncipherPassword += (char)(((int)c - movement - 65 + 26) % 26 + 65); // Agregar la letra descifrada al resultado
+                else if (char.IsDigit(c)) // Si el caracter es un número, se descifra mediante el desplazamiento
+                    uncipherPassword += (char)(((int)c - movement - 48 + 10) % 10 + 48); // Agregar el número descifrado al resultado
+                else // Si el caracter no es una letra ni un número, se agrega tal cual al resultado
+                    uncipherPassword += c;
+            uncipherPassword.Remove(uncipherPassword.Length - 1, 1);
+            return uncipherPassword; // Devolver el resultado del descifrado
+        }
+
+
+
+
+        /// <summary>
         /// Método que permite al usuario cambiar su email
         /// </summary>
         public void ChangeEmail()
@@ -132,7 +185,7 @@ namespace AppSim
                 Console.WriteLine("Formato incorrecto, por favor introduzca un email válido");
                 newEmail = Functions.ReadString("Introduzca el nuevo email: ", 1);
             }
-             SetEmail(newEmail);
+                SetEmail(newEmail);
         }
 
         public override string ToString() => $"\n\tCodigo de usuario:{GetCodUser()}\t\tNombre: {GetName()}";
