7dfb614e5744ebdf6fd2cb225481a37057a4048b
CH0P0 <gafonsoudev@gmail.com>
2023-05-03 12:09:37 +0100

    Cafeteria empezado

---
+++ OOP/Cafeteria/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafeteria
+{
+    internal class Order
+    {
+        static Dictionary<string, decimal> products = new Dictionary<string, decimal>()
+        {
+            { "sandwicht de pollo", 4 },
+            { "tortilla con cebolla", 4 },
+            { "zumo de naranja", 2 }
+
+        };
+
+        string Product { get; set; }
+        decimal Price { get; set; }
+        string Time { get; set; }
+
+        public Order(string Product, decimal Price)
+        {
+            this.Product = Product;
+            this.Price = Price;
+            Time = GetDate();
+        }
+
+        public string GetDate()
+        {
+            DateTime actualDate = DateTime.Now;
+            return actualDate.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        public static void ShowProducts()
+        {
+        }
+
+    }
+}
---
+++ OOP/Cafeteria/Program.cs
@@ -0,0 +1,26 @@
+﻿//La cafetería del centro nos ha pedido que realicemos un programa
+//que permita gestionar los pedidos que realiza el alumnado.
+//Estos pedidos se componen de una serie de productos, tantos
+//como quiera el usuario, a elegir entre los que ofrece la cafetería
+//en su menú. Los productos que contiene el menú se especificarán en el código. 
+//La cafetería tendrá que ir anotando los pedidos en una cola de
+//pedidos que solo admite un máximo de 5, de manera que si la cola está llena
+//no se podrán añadir más pedidos hasta que se sirva alguno de los pendientes. 
+//Los pedidos han de registrar un conjunto de productos y la fecha en la que
+//fue pedido (dia, mes, año y hora).
+//Los pedidos se sirven por orden de llegada, cuando el usuario así lo elija
+//en el menú, teniendo que mostrar el coste que tenía el pedido servido. 
+//El programa tendrá que permitir “hacer caja” mostrando todos los pedidos
+//servidos hasta el momento, con sus productos y precio de pedido, y el total
+//de dinero recaudado hasta el momento.
+
+namespace Cafeteria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
