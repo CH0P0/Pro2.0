fdfe39cfd54e979a3d1509feaa09f7a832d9ad90
CH0P0 <gafonsoudev@gmail.com>
2023-05-21 15:54:07 +0100

    Ejercicio 1 casi terminado

---
+++ Ejercicios701/Ejercicios701/Pages/Ejercicio2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ejercicios701.Pages
+{
+    public class Ejercicio2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
