815824ce9225970136be18d84a910b090d108cfd
CH0P0 <gafonsoudev@gmail.com>
2023-05-21 16:45:45 +0100

    Ejercicio 2 Terminado

--- Ejercicios701/Ejercicios701/Pages/Ejercicio1.cshtml.cs
+++ Ejercicios701/Ejercicios701/Pages/Ejercicio1.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ejercicios701.Pages
 {
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class Ejercicio1Model : PageModel
     {
         public void OnGet()
--- Ejercicios701/Ejercicios701/Pages/Ejercicio2.cshtml.cs
+++ Ejercicios701/Ejercicios701/Pages/Ejercicio2.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ejercicios701.Pages
 {
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class Ejercicio2Model : PageModel
     {
         public void OnGet()
