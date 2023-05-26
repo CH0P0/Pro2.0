c72129367d9872cff5cb962653874d879b121189
Guille <gafonsoudev@gmail.com>
2023-05-26 10:20:07 +0200

    ejercicio 6 empezado

--- WebApplication3/WebApplication3/Pages/RowCol.cshtml.cs
+++ WebApplication3/WebApplication3/Pages/RowCol.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication3.Pages
 {
+    [IgnoreAntiforgeryToken(Order = 1001)]
     public class RowColModel : PageModel
     {
         public void OnGet()
--- WebApplication3/WebApplication3/Program.cs
+++ WebApplication3/WebApplication3/Program.cs
@@ -1,9 +1,34 @@
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
+namespace WebApplication3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
-app.MapRazorPages();
-app.UseStaticFiles();
+            // Add services to the container.
+            builder.Services.AddRazorPages();
 
+            var app = builder.Build();
 
-app.Run();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
