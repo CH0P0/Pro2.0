b2d65f2e06a69402c6e60cff5361b89658454891
CH0P0 <gafonsoudev@gmail.com>
2023-06-01 21:23:51 +0100

    Terminado

---
+++ WebApplication3/WebApplication3/Pages/Ejercicio8.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Pages
+{
+    public class Ejercicio8Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
