8bfee10a8341afce23e15bea9eb1cda72aeaae07
CH0P0 <gafonsoudev@gmail.com>
2023-06-06 19:01:31 +0100

    Ejercicio1 casi terminado

--- ProyectoRazor/ProyectoRazor/Pages/SelectionPage.cshtml.cs
+++ ProyectoRazor/ProyectoRazor/Pages/SelectionPage.cshtml.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoRazor.Models;
 
 namespace ProyectoRazor.Pages
 {
     [IgnoreAntiforgeryToken(Order = 1001)]
     public class SelectionPageModel : PageModel
     {
+        public readonly DiscosContext Datos;
+
+        public SelectionPageModel(DiscosContext datos)
+        {
+            Datos = datos;
+        }
+
         public void OnGet()
         {
         }
